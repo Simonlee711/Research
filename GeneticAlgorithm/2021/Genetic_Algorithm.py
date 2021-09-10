df948496ff8b8fc851f019657b2d0c93b839b707
Simon Lee <simonlee711@gmail.com>
2021-09-10 15:17:22 -0700

    updated version with graphing included:

--- GeneticAlgorithm/2021/Genetic_Algorithm.py
+++ GeneticAlgorithm/2021/Genetic_Algorithm.py
@@ -2,169 +2,252 @@
 A Python module that performs the Genetic Algorithm
 '''
 __author__ = 'Simon Lee, siaulee@ucsc.edu'
-from functools import partial
-import time
-from random import random
-from collections import namedtuple
-from random import choices, randint, randrange
-from typing import Callable, List, NamedTuple, Tuple
-#from typing_extensions import get_type_hints
+import random
+import copy
+import math
+import matplotlib.pyplot as plt
 
-Genome = List[int]
-Population = List[Genome]
-FitnessFunc = Callable[[Genome], int]
-PopulationFunc = Callable[[], Population]
-SelectionFunc = Callable[[Population, FitnessFunc], Tuple[Genome, Genome]]
-CrossoverFunc = Callable[[Genome, Genome], Tuple[Genome, Genome]]
-MutationFunc = Callable[[Genome], Genome]
-Thing = namedtuple('Thing', ['name', 'value', 'weight'])
+#inititialization
+POPULATION_SIZE = 20
+CITIES_SIZE = 20
+TOUR_SIZE = 21
+NUM_EXECUTIONS = 9999
+population = []
+x = []
+y = []
 
-things = [
-    Thing('Laptop', 500, 2200),
-    Thing('Headphones', 150, 160),
-    Thing('Coffee Mug', 60, 350),
-    Thing('Notepad', 40, 333),
-    Thing('Water Bottle', 30, 192)
-]
+tour = [[0 for x in range(TOUR_SIZE)] for y in range(TOUR_SIZE)]
+dCidade = [[0 for x in range(POPULATION_SIZE)] for y in range(POPULATION_SIZE)]
+distances = [0 for x in range(POPULATION_SIZE)]
+parentsOne = None
+parentsTwo = None
+costByExecution = []
 
-more_things = [
-    Thing('Mints', 5, 25),
-    Thing('Socks', 10, 38),
-    Thing('Tissues', 15, 80),
-    Thing('Phone', 500, 200),
-    Thing('Baseball Cap', 100, 70)
-] + things
+''' Generates the first population'''
+def generateFirstPopulation():
+    # for each position, generates a new possible path
+    for _ in range(1, POPULATION_SIZE + 1):
+        generatePossiblePath()
 
 
-def generate_genome(length: int) -> Genome:
-    '''
-    creates a set of genomes that randomly assigns binary values 0 or 1 to the length of the genome
-    '''
-    return choices([0,1], k=length)
+''' Generate a new possible path for a population'''
+def generatePossiblePath():
+    path = []
+    for _ in range(1, CITIES_SIZE + 1):
+        # generates a new number between 1 - 20
+        randomNum = random.randint(1,20)
+        # while the generated number exists in the list, generates a new one
+        while(numberExistsInPath(path, randomNum)):
+            randomNum = random.randint(1, 20)
+        path.append(randomNum)
+    population.append(path)
 
 
-def generate_population(size: int, genome_length: int) -> Population:
-    '''
-    creates a population of genomes that were generated capped by some size and genome length
-    '''    
-    return [generate_genome(genome_length) for _ in range(size)]
+''' Method to verify if the number is already in the path'''
+def numberExistsInPath(path, number):
+    for i in path:
+        if i  == number:
+            return True
+    return False
 
 
-def fitness(genome: Genome, things: List[Thing], weight_limit: int) -> int:
-    '''
-    This evaluates a solution and sees its fitness level and whether it succeeds the limit that is established
-    '''
-    if len(genome) != len(things):
-        raise ValueError("genome and things must be of same length")
+''' Generates the X and Y arrays which represents the distances in the x 
+and y axis used to calculate the identity matrix in the fitnress function'''
+def generateXandY():
+    for _ in range(CITIES_SIZE):
+        randomNumber = random.random()
+        randomNumber = round(randomNumber, 2)
+        x.append(randomNumber)
 
-    weight = 0
-    value = 0
+        randomNumber = random.random()
+        randomNumber = round(randomNumber, 2)
+        y.append(randomNumber)
 
-    for i, thing in enumerate(things):
-        if genome[i] == 1:
-            weight += thing.weight
-            value += thing.value
+'''
+Generates the tour matrix, which is the same matrix as the population
+but with the first column duplicated at the end of it, afterall, the traveler
+always have to arrive at the same place of where it started. The hamiltonian cycle
+'''
+def generateTour():
+    global tour
+    tour = copy.deepcopy(population)
+    for ways in tour:
+        first = ways[0]
+        ways.append(first)
 
-            if weight > weight_limit:
-                return 0
-    
-    return value
-
-
-def selection_pair(population: Population, fitness_func: FitnessFunc) -> Population:
-    '''
-    the selection of two parent solutions randomly to produce to solutions
-    '''
-    return choices(
-        population=population,
-        weights=[fitness_func(genome) for genome in population],
-        k=2
-    )
-
-
-def single_point_crossover(a: Genome, b: Genome) -> Tuple[Genome,Genome]:
-    '''
-    performs the exchange of n length bits to be swapped to produce new solutions
-    '''
-    if len(a) != len(b):
-        raise ValueError("genome and things must be of same length")
-    
-    length = len(a)
-    if length < 2:
-        return a, b
-
-    p = randint(1, length -1)
-    return a[0:p] + b[p:], b[0:p] + a[p:]
+'''
+Generates an array with the sum of each path in the population array based on the tour matrix
+'''
+def calculateDistances():
+    global distances
+    distances = [0 for x in range(CITIES_SIZE)]
+    for i in range(len(population)):
+        for j in range(len(population[i])):
+            firstPos = 19 if tour[i][j] == 20 else tour[i][j]
+            secondPos = 19 if tour[i][j+1] == 20 else tour[i][j+1]
+            distances[i] += round(dCidade[firstPos][secondPos], 4)
+    dict_dist = {i: distances[i] for i in range(0, len(distances))}
+    distances = copy.deepcopy(dict_dist)
+    return sorted(distances.items(), key=lambda kv: kv[1])
 
 
-def mutation(genome: Genome, num: int = 1, probability: float = 0.5) -> Genome:
-    for _ in range(num):
-        index = randrange(len(Genome))
-        genome[index] = genome[index] if random() > probability else abs(genome[index] - 1)
-    return genome
+'''
+Generates the identity matrux (dCidade) based on the x and y arrays
+and then call the calculateDistances() method to generate the array with the sum
+of each path to user later in the cycle process
+'''
+def FitnessFunction():
+    for i in range(len(population)):
+        for j in range(len(population)):
+            dCidade[i][j] = round(math.sqrt(((x[i] -x[j]) ** 2) + ((y[i] - y[j]) ** 2)), 4)
+    return calculateDistances()
 
 
-def genome_to_things(genome: Genome, things: List[Thing]) -> List[Thing]:
-    result = []
-    for i, thing in enumerate(things):
-        if genome[i]  == 1:
-            result += [thing.name]
-    return result
+'''
+Performs the roulette function, generating two arrays with 5 parents each,
+which will be used later to do the cycle process
+'''
+def rouletteFunction(sorted_x):
+    global parentsOne
+    global parentsTwo
+    arr = []
+    rouletteArr =[]
+    for i in range(10):
+        arr.append(sorted_x[i][0])
+    for j in range(len(arr)):
+        for _ in range(10 - j):
+            rouletteArr.append(arr[j])
+    parentsOne = createParents(rouletteArr)
+    parentsTwo = createParents(rouletteArr)
 
-def run_evolution(
-    populate_func: PopulationFunc,
-    fitness_func: FitnessFunc,
-    fitness_limit: int,
-    selection_func: SelectionFunc = selection_pair,
-    crossover_func: CrossoverFunc = single_point_crossover,
-    mutation_func: MutationFunc = mutation,
-    generation_limit: int = 100
-) -> Tuple[Population, int]:
-    '''
-    main module responsible for putting all the functions together
-    '''
-    population = populate_func()
 
-    for i in range(generation_limit):
-        population = sorted(
-            population,
-            key=lambda genome:fitness_func(genome),
-            reverse=True
-        )
+'''
+Auxiliary method used in the rouletteFunction() to generate the two parents array
+'''
+def createParents(rouletteArr):
+    parentArr =[]
+    for _ in range(5):
+        parentArr.append(rouletteArr[random.randint(0, 54)])
+    return parentArr
 
-        if fitness_func(population[0]) >= fitness_limit:
-            break
+'''
+makes the swap between two cities in the path with a 2% chance of mutation
+'''
+def mutate(matrix):
+    for i in range(0, len(matrix)):
+        for _ in range(0, len(matrix[i])):
+            ranNum = random.randint(1,100)
+            if ranNum >= 1 and ranNum <= 2:
+                indexOne = random.randint(0,19)
+                indexTwo = random.randint(0,19)
+                auxOne = matrix[i][indexOne]
+                auxTwo = matrix[i][indexTwo]
+                matrix[i][indexOne] = auxTwo
+                matrix[i][indexTwo] = auxOne
 
-        next_generation = population[0:2]
 
-        for j in range(int(len(population) / 2) - 1):
-            parents = selection_func(population, fitness_func)
-            offspring_a, offspring_b = crossover_func(parents[0], parents[1])
-            offspring_a = mutation(offspring_a)
-            offspring_b = mutation(offspring_b)
-            next_generation += [offspring_a, offspring_b]
+'''
+Method used in the cycle to see if theres any duplication between cities
+'''
+def hasDuplicity(auxArray, usedIndexes):
+    for i in range(len(auxArray)):
+        for j in range(i, len(auxArray)):
+            if i != j  and auxArray[i] == auxArray[j]:
+                if i in usedIndexes:
+                    return j
+                else:
+                    return i
+    return -1
+
+
+'''
+Method that runs the cycle
+1. For each two children in the children array, it makes a random swap between
+   two children until theres no duplicated element
+2. mutate the children that were generated
+3. adds the children in the population array
+'''
+
+def Cycle(sorted_x):
+    global population
+    children = []
+
+    for i in range(5):
+        parentsOneAux = parentsOne[i]
+        parentsTwoAux = parentsTwo[i]
+        usedIndexes = []
+
+        randomIndex = random.randint(0, POPULATION_SIZE - 1)
+        usedIndexes.append(randomIndex)
+
+        childOne = copy.deepcopy(population[parentsOneAux])
+        childTwo = copy.deepcopy(population[parentsTwoAux])
+
+        valAuxOne = childOne[randomIndex]
+        valAuxTwo = childTwo[randomIndex]
+
+        childOne[randomIndex] = valAuxTwo
+        childTwo[randomIndex] = valAuxOne
+
+        while(hasDuplicity(childOne, usedIndexes) != -1):
+            newIndex = hasDuplicity(childOne, usedIndexes)
+            usedIndexes.append(newIndex)
+
+            valAuxOne = childOne[newIndex]
+            valAuxTwo = childTwo[newIndex]
+
+            childOne[newIndex] = valAuxTwo
+            childTwo[newIndex] = valAuxOne
         
-        population = next_generation
-    population = sorted(
-        population,
-        key=lambda genome: fitness_func(genome),
-        reverse=True
-    )
+        #after generating the children, add them in the childrens array
+        children.append(childOne)
+        children.append(childTwo)
+
+    #mutate children
+    mutate(children)
+
+    #make a temporary copy of the population before changing it
+    tempPopulation = copy.deepcopy(population)
+
+    for i in range(10):
+        population[i] = copy.deepcopy(tempPopulation[sorted_x[i][0]])
     
-    return population, i
+    #adjust population
+    for j in range(10, POPULATION_SIZE):
+        population[j] = copy.deepcopy(children[j - 10])
 
-start = time.time()
-population, generation = run_evolution(
-    populate_func=partial(
-        generate_population, size = 10, genome_length=len(things)
-    ),
-    fitness_func=partial(
-        fitness, things=things, weight_limit=3000
-    ),
-    fitness_limit=740,
-    generation_limit=100
-)
-end = time.time()
+def main():
+    # runs once. generates a population, x and y, and tour matrix
+    generateFirstPopulation()
+    generateXandY()
+    generateTour()
 
-print(f"number of generations: {generation}")
-print(f"best solution: {genome_to_things(population[0], things)}")
+    #runs in a loop 0 - 9999
+    for _ in range(NUM_EXECUTIONS):
+        sorted_x = FitnessFunction()
+        rouletteFunction(sorted_x)
+        Cycle(sorted_x)
+        generateTour() # generate the tour matrix again as the population is updated
+        costByExecution.append(sorted_x[0][1]) # append the cost to the array of costs
+
+    # generates a fitness value for the last population
+    sorted_x = FitnessFunction()
+
+    print('Total Population:' + POPULATION_SIZE)
+    print('Mutation Probabiltiy: 2%')
+    print('Number of Cities:' + CITIES_SIZE)
+    print('Optimal path cost: %s' % sorted_x[0][1])
+    print('Best Route' + population[0])
+
+    #show the path through a graph
+    plt.plot(tour[0])
+    plt.plot(tour[0], 'ro')
+    plt.axis([0,20,0,20])
+    plt.show()
+
+    # show the cost graph
+    plt.plot(costByExecution)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
